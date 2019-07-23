732e53e65af701df66e283729c3071522df7459e
Mario Román <mromang08@gmail.com>
2019-07-23 12:23:56 +0100

    Update Vitrea with examples and optics

--- Example.hs
+++ Example.hs
@@ -24,7 +24,11 @@ module Example where
 
 import           Vitrea
 
-
+import           Control.Arrow
+import           Data.Char
+import           Data.Foldable
+import           Data.Functor.Compose
+import           Text.Printf
 
 -- The data structures.
 data PostalAddr = PostalAddr
@@ -33,98 +37,135 @@ data PostalAddr = PostalAddr
   , city'   :: String
   }
 
+data EmailAddr = EmailAddr
+  { user'   :: String
+  , domain' :: String
+  }
+
+data MailList = MailList
+  { unMailList :: [(Name, Address, Subscription)]
+  }
+
 type Address = String
 type Street = String
 type ZipCode = String
+type Domain = String
+type Name = String
+data Subscription = Daily | Monthly | Weekly deriving (Show)
+
 
 instance Show PostalAddr where
   show (PostalAddr s z c) =
-    "   Street:   " ++ s ++
-    "\n   Code:     " ++ z ++
-    "\n   City:     " ++ c
+    " Street:   " ++ s ++
+    "\n  Code:     " ++ z ++
+    "\n  City:     " ++ c
+
+instance Show EmailAddr where
+  show (EmailAddr u d) =
+    " User:" ++ u ++
+    " Domain:" ++ d
 
 
--- A lens that allows us to see the street from an adress.
--- And a prism that tries to match an address into a postal one.
+someMailingList :: MailList
+someMailingList = MailList
+  [ ("Turing, Alan" , "turing@princeton.edu" , Daily)
+  , ("Noether, Emily" , "emmynoether@fau.eu" , Monthly)
+  , ("Gauss, Carl F." , "gauss@goettingen.de" , Weekly)
+  ]
+
+someAddress :: Address
+someAddress = "High Street, OX46NA, Oxford"
+
+
+-- Lenses.
+-- We are explicitly writing down the view/update.
 zipcode :: ProfLens' PostalAddr ZipCode
-zipcode = mkLens $ ExOptic viewCode updateCode
-  where
-    viewCode    p      = (p , code' p)
-    updateCode (p , s) = p {code' = s}
+zipcode = mkLens $ ExOptic (id &&& code') (\(p , s) ->  p {code' = s})
 
 street :: ProfLens' PostalAddr Street
-street = mkLens $ ExOptic viewStreet updateStreet
-  where
-    viewStreet    p      = (p , street' p)
-    updateStreet (p , s) = p {street' = s}
+street = mkLens $ ExOptic (id &&& street') (\(p , s) -> p {street' = s})
 
+city :: ProfLens' PostalAddr Street
+city = mkLens $ ExOptic (id &&& city') (\(p , s) -> p {city' = s})
 
+domain :: ProfLens' EmailAddr Domain
+domain = mkLens $ ExOptic (id &&& domain') (\(p , s) -> p {domain' = s})
+
+-- Prisms.
 postal :: ProfPrism' Address PostalAddr
 postal = mkPrism $ ExOptic matchPostal buildPostal
   where
     matchPostal :: Address -> Either Address PostalAddr
-    matchPostal a = case matchPostal' a of
-                      Nothing -> Left a
-                      Just x  -> Right x
-    matchPostal' :: Address -> Maybe PostalAddr
-    matchPostal' a = do
-      (street, b) <- readUntilComma a
-      (city, c) <- readUntilComma (tail $ tail b)
+    matchPostal a = maybe (Left a) Right $ do
+      (street, b) <- readUntil ',' a
+      (city, c)   <- readUntil ',' (tail $ tail b)
       return $ PostalAddr street city (tail $ tail c)
-        where
-          readUntilComma a =
-             if elem ',' a
-               then Just (takeWhile (/= ',') a, dropWhile (/= ',') a)
-               else Nothing
-
     buildPostal :: Either Address PostalAddr -> Address
     buildPostal (Left a)                   = a
     buildPostal (Right (PostalAddr s t c)) = s ++ ", " ++ t ++ ", " ++ c
 
-
--- Now some combinators.
-data CrAff s t a b = CrAff
-  { original :: s
-  , get      :: Maybe a
-  , set      :: Maybe (b -> t)
-  }
-
-instance (Show s, Show a) => Show (CrAff s t a b) where
-  show (CrAff o Nothing t)  = "Nothing"
-  show (CrAff o (Just a) t) = show a
-
-(^.) :: s -> ProfAffine s t a b -> CrAff s t a b
-s ^. p = CrAff s (getter (crOptic p) s) (setter (crOptic p) s)
+email :: ProfPrism' Address EmailAddr
+email = mkPrism $ ExOptic matchEmail buildEmail
   where
-    getter :: ExAffine s t a b -> s -> Maybe a
-    getter (ExOptic l _) = either (const Nothing) (Just . snd) . unAff . l
-    setter :: ExAffine s t a b -> s -> Maybe (b -> t)
-    setter (ExOptic l r) =
-      either (const Nothing)
-        (Just . curry (r . Aff . Right) . fst) . unAff . l
-
-(<~) :: CrAff s s a a -> a -> s
-(<~) (CrAff s _ Nothing)  b = s
-(<~) (CrAff _ _ (Just f)) b = f b
-
-(<~~) :: CrAff s s a a -> (a -> a) -> s
-(<~~) (CrAff s _ Nothing) g         = s
-(<~~) (CrAff s Nothing _) g         = s
-(<~~) (CrAff _ (Just a) (Just f)) g = f $ g a
+    matchEmail :: Address -> Either Address EmailAddr
+    matchEmail a = maybe (Left a) Right $ do
+      (u, d) <- readUntil '@' a
+      return $ EmailAddr u $ tail d
+    buildEmail :: Either Address EmailAddr -> Address
+    buildEmail (Left a)                = a
+    buildEmail (Right (EmailAddr u d)) = u ++ "@" ++ d
 
 
-infixr 8 ^.
-infixl 6 <~
-infixl 6 <~~
+-- Traversals.
+mails :: ProfTraversal' MailList Address
+mails = mkTraversal $ ExOptic (\m -> Compose (m , fst $ extractAddr m)) set
+  where
+    extractAddr :: MailList -> ([Address], [Address] -> MailList)
+    extractAddr (MailList m) = (map (\(_,a,_) -> a) m, MailList . fmap (\((n,_,s),a) -> (n,a,s)) . zip m)
+
+    set :: (Compose ((,) MailList) []) Address -> MailList
+    set (Compose (m , a)) = (snd $ extractAddr m) a
+
+instance Show MailList where
+  show = unlines
+    . ((:) (printf "| %20s | %30s | %10s |" "Name" "Email" "Frequency"))
+    . ((:) (replicate 70 '-'))
+    . fmap (\(n,a,s) -> printf "| %20s | %30s | %10s |" n a (show s)) . unMailList
 
 
--------------
--- EXAMPLE --
--------------
---  "High Street, OX46NA, Oxford"^.postal.street
+
+
+
+---------------
+-- EXAMPLE 1 --
+---------------
+--  "15 Parks Rd, OX1 3QD, Oxford"^.postal.street
 --    >> "High Street"
 --  "High Street, OX46NA, Oxford"^.postal.street <~ "Banbury Road"
 --    >> "Banbury Road, OX46NA, Oxford"
---  "High Street, OX46NA, Oxford"^.postal.zipcode <~~ (++ "-UK")
---    >> "High Street, OX46NA-UK, Oxford"
--------------
+--  "High Street, OX46NA, Oxford"^.postal.zipcode <~~ (++ "(UK)")
+--    >> "High Street, OX46NA(UK), Oxford"
+---------------
+
+
+---------------
+-- EXAMPLE 2 --
+---------------
+-- someMailingList^..mails.email.domain <~~~ uppercase
+--
+-- >> |                 Name |                          Email |  Frequency |
+-- >> ----------------------------------------------------------------------
+-- >> |         Turing, Alan |           turing@PRINCETON.EDU |      Daily |
+-- >> |       Noether, Emily |             emmynoether@FAU.EU |    Monthly |
+-- >> |       Gauss, Carl F. |            gauss@GOETTINGEN.DE |     Weekly |
+---------------
+
+-- Auxiliary functions.
+uppercase, lowercase :: String -> String
+uppercase = fmap toUpper
+lowercase = fmap toLower
+
+readUntil :: Char -> String -> Maybe (String , String)
+readUntil c a = if elem c a
+  then Just (takeWhile (/= c) a, dropWhile (/= c) a)
+  else Nothing
--- Vitrea.hs
+++ Vitrea.hs
@@ -22,15 +22,13 @@
 
 module Vitrea where
 
+import           Control.Monad.State
+import           Data.Foldable
 import           Data.Functor.Compose
 import           Data.Functor.Identity
 import           Data.Profunctor
 import           Data.Void
-import           GHC.Exts
-
-type family Arg  (x :: * -> *) where Arg  (f a)   = a
-type family Arg1 (x :: * -> *) where Arg1 (f a b) = a
-type family Arg2 (x :: * -> *) where Arg2 (f a b) = b
+import           GHC.Exts              hiding (toList)
 
 
 
@@ -74,16 +72,17 @@ mkOptic (ExOptic l r) = dimap l r . (action @mon)
 ------------
 -- Lenses.
 class (f ~ ((,) (Arg f))) => Product f
+class (f ~ ((,) (Arg f))) => Lens f
 instance Product ((,) a)
 
 instance Tensorial Product where
   idOptic = ExOptic ((,) ()) snd
   multOptic (ExOptic l r) = ExOptic (\(d,s) -> ((d , fst (l s)), snd (l s))) (\((d,c),b) -> (d,r (c,b)))
 
-type ExLens s t a b   = ExOptic   Product a b s t
+type ExLens   s t a b = ExOptic   Product a b s t
 type ProfLens s t a b = ProfOptic Product a b s t
-type ExLens' t a      = ExOptic   Product a a t t
-type ProfLens' t a    = ProfOptic Product a a t t
+type ExLens'    t a   = ExOptic   Product a a t t
+type ProfLens'  t a   = ProfOptic Product a a t t
 
 mkLens :: forall a t . ExLens' t a -> ProfLens' t a
 mkLens = mkOptic
@@ -98,10 +97,10 @@ instance Tensorial Sum where
     (either (Left . Left) (either (Left . Right) Right . l))
     (either (either Left (Right . r . Left)) (Right . r . Right))
 
-type ExPrism s t a b   = ExOptic   Sum a b s t
+type ExPrism   s t a b = ExOptic   Sum a b s t
 type ProfPrism s t a b = ProfOptic Sum a b s t
-type ExPrism' t a   = ExOptic   Sum a a t t
-type ProfPrism' t a   = ProfOptic   Sum a a t t
+type ExPrism'    t a   = ExOptic   Sum a a t t
+type ProfPrism'  t a   = ProfOptic Sum a a t t
 
 mkPrism :: forall a t . ExPrism' t a -> ProfPrism' t a
 mkPrism = mkOptic
@@ -126,6 +125,14 @@ instance Tensorial Traversable where
 
 type ExTraversal   s t a b = ExOptic   Traversable a b s t
 type ProfTraversal s t a b = ProfOptic Traversable a b s t
+type ExTraversal'    t a   = ExOptic   Traversable a a t t
+type ProfTraversal'  t a   = ProfOptic Traversable a a t t
+
+mkTraversal :: forall a t . ExTraversal' t a -> ProfTraversal' t a
+mkTraversal = mkOptic
+
+instance (Profunctor p , Tambara Traversable p) => Tambara AffineTransf p where
+  action = dimap (Compose . unAff) (Aff . getCompose) . action @Traversable
 
 
 -- Setters.
@@ -192,3 +199,80 @@ instance Achromatic (Achl a)
 -- instance Tensorial IsIdentity where
 --   idOptic = ExOptic coerce runIdentity
 --   multOptic (ExOptic l r) = ExOptic _ _
+
+
+
+
+-----------------
+-- COMBINATORS --
+-----------------
+-- Now some combinators.
+data CrAff s t a b = CrAff
+  { aoriginal :: s
+  , aget      :: Maybe a
+  , aset      :: Maybe (b -> t)
+  }
+
+instance (Show s, Show a) => Show (CrAff s t a b) where
+  show (CrAff o Nothing t)  = "Nothing"
+  show (CrAff o (Just a) t) = show a
+
+(^.) :: s -> ProfAffine s t a b -> CrAff s t a b
+s ^. p = CrAff s (getter (crOptic p) s) (setter (crOptic p) s)
+  where
+    getter :: ExAffine s t a b -> s -> Maybe a
+    getter (ExOptic l _) = either (const Nothing) (Just . snd) . unAff . l
+    setter :: ExAffine s t a b -> s -> Maybe (b -> t)
+    setter (ExOptic l r) =
+      either (const Nothing)
+        (Just . curry (r . Aff . Right) . fst) . unAff . l
+
+(<~) :: CrAff s s a a -> a -> s
+(<~) (CrAff s _ Nothing)  b = s
+(<~) (CrAff _ _ (Just f)) b = f b
+
+(<~~) :: CrAff s s a a -> (a -> a) -> s
+(<~~) (CrAff s _ Nothing) g         = s
+(<~~) (CrAff s Nothing _) g         = s
+(<~~) (CrAff _ (Just a) (Just f)) g = f $ g a
+
+
+data TrAff s t a b = TrAff
+  { tor  :: s
+  , tget :: [a]
+  , tset :: [b] -> t
+  }
+
+
+(<~~~) :: TrAff s s a a -> (a -> a) -> s
+(<~~~) (TrAff _ a f) g = f $ fmap g a
+
+
+instance (Show s, Show a) => Show (TrAff s t a b) where
+  show = show . tget
+
+(^..) :: s -> ProfTraversal s t a b -> TrAff s t a b
+s ^.. p = TrAff s (getter (crOptic p) s) (setter (crOptic p) s)
+  where
+    getter :: ExTraversal s t a b -> s -> [a]
+    getter (ExOptic l _) = toList . l
+    setter :: ExTraversal s t a b -> s -> [b] -> t
+    setter (ExOptic l r) s list = r . fst . ($ 0) . runState . mapM (counter list) . l $ s
+      where
+        counter :: [b] -> a -> State Int b
+        counter list _ = do
+          n <- get
+          modify (+1)
+          return $ list !! n
+
+infixr 8 ^.
+infixr 8 ^..
+infixl 6 <~
+infixl 6 <~~
+infixl 6 <~~~
+
+
+-- Auxiliary functions
+type family Arg  (x :: * -> *) where Arg  (f a)   = a
+type family Arg1 (x :: * -> *) where Arg1 (f a b) = a
+type family Arg2 (x :: * -> *) where Arg2 (f a b) = b
